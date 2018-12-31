fd99c484ef2bc85c65483e3fa5c3fc763d0ca325
Te-Chi Liu <liuderchi@gmail.com>
2018-12-31 15:24:51 +0800

    [3] chore: introduce parcel, parcel-plugin-static-files-copy
    - add icon, manifest.json from examples

---
+++ omnibox-parcel/src/favoriteSites.js
@@ -0,0 +1,19 @@
+export const FAVORITE_SITES = {
+  GH: [
+    {
+      name: 'liuderchi/ide-css',
+      // TODO tags
+      url: 'https://github.com/liuderchi/ide-css',
+    },
+    {
+      name: 'liuderchi/ide-html',
+      url: 'https://github.com/liuderchi/ide-html',
+    },
+    {
+      name: 'liuderchi/ide-yaml',
+      url: 'https://github.com/liuderchi/ide-yaml',
+    },
+  ],
+};
+
+export default FAVORITE_SITES;
--- omnibox-parcel/src/index.js
+++ omnibox-parcel/src/index.js
@@ -1,9 +1,52 @@
-import "./styles.css";
+import FAVORITE_SITES from './favoriteSites';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const getCurrentTabId = callback => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    if (callback) callback(tabs.length ? tabs[0].id : null);
+  });
+};
+const openUrlCurrentTab = url => {
+  getCurrentTabId(tabId => {
+    chrome.tabs.update(tabId, { url });
+  });
+};
+const openUrlNewTab = url => {
+  chrome.tabs.create({ url });
+};
+
+const mapSiteToSuggestion = (site) /* :{name: string, url: string} */ => ({
+  content: site.url,
+  description: `🐙 ${site.name} - ${site.url}`,
+});
+const getGitHubSearchSuggestion = (keyword) /* :string */ => ({
+  content: `https://github.com/search?q=${keyword}&ref=opensearch`,
+  description: `🔎 Search '${keyword}' in GitHub`,
+});
+
+// add listener to omnibox
+chrome.omnibox.onInputChanged.addListener((inputText, suggest) => {
+  console.log('inputChanged: ' + inputText);
+  if (!inputText) return;
+  if (/^gh/i.test(inputText)) {
+    const matchRes = inputText.match(/^gh\W+([\w_-]+)/);
+    if (matchRes !== null) {
+      const keyword = matchRes[1];
+      const filteredSites = FAVORITE_SITES.GH.filter(
+        site => site.name.indexOf(keyword) > -1,
+      );
+      suggest(
+        filteredSites
+          .map(mapSiteToSuggestion)
+          .concat(getGitHubSearchSuggestion(keyword)),
+      );
+    }
+  } else if (/^b/i.test(inputText)) {
+    // TODO search in chrome bookmarks
+  }
+});
+chrome.omnibox.onInputEntered.addListener(inputText => {
+  console.log('inputEntered: ' + inputText);
+  if (!inputText) return;
+
+  openUrlCurrentTab(inputText);
+});
